ba8e4b7c4b2888b4d5cc4416972dae382214e7c8
pusnis <pusnis@gmail.com>
2022-04-09 10:12:27 +0300

    ADd MyCameraActivity and MAterilaButton Takephoto from MainActivity

--- app/src/main/java/lt/pusnis/multibarcodereader/MainActivity.java
+++ app/src/main/java/lt/pusnis/multibarcodereader/MainActivity.java
@@ -2,13 +2,35 @@ package lt.pusnis.multibarcodereader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialButton mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpBtnTakPhoto();
+    }
+
+    private void setUpBtnTakPhoto() {
+        mBtn = findViewById(R.id.mBtn);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, MyCameraActivity.class);
+                startActivity(intent);
+
+            }
+        };
     }
 }
---
+++ app/src/main/java/lt/pusnis/multibarcodereader/MyCameraActivity.java
@@ -0,0 +1,14 @@
+package lt.pusnis.multibarcodereader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_camera);
+    }
+}
