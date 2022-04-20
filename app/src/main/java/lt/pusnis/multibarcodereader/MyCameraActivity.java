1a26a2fea8d476209355a1169f21462e612aa1c5
pusnis <pusnis@gmail.com>
2022-04-20 15:02:27 +0300

    Remove Image Capture methods

--- app/src/main/java/lt/pusnis/multibarcodereader/MyCameraActivity.java
+++ app/src/main/java/lt/pusnis/multibarcodereader/MyCameraActivity.java
@@ -1,22 +1,11 @@
 package lt.pusnis.multibarcodereader;
 
-import static android.provider.CalendarContract.CalendarCache.URI;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.ImageProxy;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
 import android.content.Intent;
@@ -26,11 +15,8 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.CalendarContract;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.util.Size;
-import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -38,7 +24,6 @@ import android.widget.ImageView;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
@@ -50,7 +35,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class MyCameraActivity extends AppCompatActivity {
 
@@ -58,15 +42,11 @@ public class MyCameraActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_SELECT = 2;
     private static final String TAG = "Capture_Save_Show_Image";
     private ImageView imageView;
-    private Button captureButton;
-    private Button selectButton;
     private InputImage image;
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
-    private PreviewView previewView;
-
+    private Uri selectedImage;
 
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
-    String currentPhotoPath;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,25 +57,15 @@ public class MyCameraActivity extends AppCompatActivity {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
         }
 
-        //imageView = this.findViewById(R.id.imageView1);
+        imageView = (ImageView) this.findViewById(R.id.imageView1);
 
-        setCaptureButton();
         setSelectButton();
 
     }
 
-    private void setCaptureButton() {
-        selectButton = (Button) this.findViewById(R.id.button1);
-        selectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                captureImage();
-            }
-        });
-    }
 
     private void setSelectButton() {
-        captureButton = (Button) this.findViewById(R.id.button2);
+        Button captureButton = (Button) this.findViewById(R.id.button2);
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -104,10 +74,7 @@ public class MyCameraActivity extends AppCompatActivity {
 
                 Intent intent = new Intent();
                 intent.setType("image/*");
-//                intent.setAction(Intent.ACTION_GET_CONTENT);
-//                startActivityForResult(Intent.createChooser(intent, "Select Picture", PICK_IMAGE);
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-
 
                 startActivityForResult(intent, REQUEST_IMAGE_SELECT );
                 writeLog("Select picture intent opened.");
@@ -115,143 +82,57 @@ public class MyCameraActivity extends AppCompatActivity {
         });
     }
 
-    private void galleryAddPic() {
-        writeLog("galleryAddPic() - start; filePAth: " + currentPhotoPath);
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(currentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
-        writeLog("galleryAddPic() - finsh");
-    }
-
-    private void captureImage() {
-
-        previewView = this.findViewById(R.id.imageView1);
-
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-        cameraProviderFuture.addListener(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                    bindImageAnalysis(cameraProvider);
-                } catch (ExecutionException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, ContextCompat.getMainExecutor(this));
 
 
-
-
-
-
-    }
-
-    private void bindImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
-        ImageAnalysis imageAnalysis =
-                new ImageAnalysis.Builder().setTargetResolution(new Size(1280, 720))
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
-        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
-            @Override
-            public void analyze(@NonNull ImageProxy image) {
-                image.close();
-            }
-        });
-        OrientationEventListener orientationEventListener = new OrientationEventListener(this) {
-            @Override
-            public void onOrientationChanged(int orientation) {
-                //textView.setText(Integer.toString(orientation));
-            }
-        };
-        orientationEventListener.enable();
-        Preview preview = new Preview.Builder().build();
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,
-                imageAnalysis, preview);
-
-    }
-
-    private void captureImage_old() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-
-//        try {
-//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//        } catch (ActivityNotFoundException e) {
-//            Log.e("REQUEST_IMAGE_CAPTURE", e.toString());
-//        }
-        writeLog("captureImage() - Start");
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                Log.e("REQUEST_IMAGE_CAPTURE", ex.toString());
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-            //writeLog("captureImage() - Finish");
-            galleryAddPic();
-        }
-
-
-    }
-
-    private File createImageFile() throws IOException {
-
-        writeLog("createImageFile() - start");
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES); //
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        writeLog("createImageFile() - finish; filePath: " + currentPhotoPath);
-        return image;
-    }
+//    private void captureImage_01() {
+//        ImageCapture imageCapture =
+//                new ImageCapture.Builder()
+//                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+//                        .build();
+//
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+//        ImageCapture.OutputFileOptions outputFileOptions =
+//                new ImageCapture.OutputFileOptions.Builder(new File(imageFileName)).build();
+//
+//        imageCapture.takePicture(outputFileOptions, cameraExecutor,
+//                new ImageCapture.OnImageSavedCallback() {
+//                    @Override
+//                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+//                        // insert your code here.
+//                    }
+//                    @Override
+//                    public void onError(ImageCaptureException error) {
+//                        // insert your code here.
+//                    }
+//                }
+//        );
+//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         writeLog("onActivityResult() - start");
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        writeLog("Intent : "+ requestCode + " resultCode: "+resultCode ) ;
+        if (  requestCode == REQUEST_IMAGE_SELECT && resultCode == RESULT_OK) {
+            selectedImage = data.getData();
 
-
-            writeLog("onActivityResult() - finish. currentPhotoPath : "+ currentPhotoPath);
-        }
-        if (requestCode == REQUEST_IMAGE_SELECT && resultCode == RESULT_OK) {
-
-            //Bitmap photo = (Bitmap) data.getExtras().get("data");
-            //imageView.setImageBitmap(photo);
-            Uri selectedImage = data.getData();
-
+            writeLog(requestCode + " selectedImage : "+ selectedImage);
             try {
                 image = InputImage.fromFilePath(getApplicationContext(), selectedImage);
-//                imageView.setImageURI(selectedImage);
+                imageView.setImageURI(selectedImage);
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            writeLog("onActivityResult() - finish. currentPhotoPath : "+ currentPhotoPath);
+            writeLog("onActivityResult() - finish. ");
 
+        } if (  requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK ){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+            //image = InputImage.fromBitmap(bitmap,0);
+
+            writeLog(requestCode + " Bitmap.");
         }
         getBarCodes();
     }
