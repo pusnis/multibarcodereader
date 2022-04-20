512936010f2d909318aef86e04e4eef9f23049ba
pusnis <pusnis@gmail.com>
2022-04-20 17:42:07 +0300

    Remove unused imports

--- app/src/main/java/lt/pusnis/multibarcodereader/MyCameraActivity.java
+++ app/src/main/java/lt/pusnis/multibarcodereader/MyCameraActivity.java
@@ -4,18 +4,15 @@ package lt.pusnis.multibarcodereader;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.view.PreviewView;
-import androidx.core.content.FileProvider;
+
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -93,33 +90,6 @@ public class MyCameraActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
-//    private void captureImage_01() {
-//        ImageCapture imageCapture =
-//                new ImageCapture.Builder()
-//                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
-//                        .build();
-//
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "JPEG_" + timeStamp + "_";
-//        ImageCapture.OutputFileOptions outputFileOptions =
-//                new ImageCapture.OutputFileOptions.Builder(new File(imageFileName)).build();
-//
-//        imageCapture.takePicture(outputFileOptions, cameraExecutor,
-//                new ImageCapture.OnImageSavedCallback() {
-//                    @Override
-//                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
-//                        // insert your code here.
-//                    }
-//                    @Override
-//                    public void onError(ImageCaptureException error) {
-//                        // insert your code here.
-//                    }
-//                }
-//        );
-//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
