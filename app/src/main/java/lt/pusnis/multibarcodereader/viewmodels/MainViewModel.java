87e143b9f2e861a637876ba5fc2cde4db2f851d3
pusnis <pusnis@gmail.com>
2022-04-22 19:51:51 +0300

    postValue(response.body().getFormatData()) to MutableLiveData

--- app/src/main/java/lt/pusnis/multibarcodereader/viewmodels/MainViewModel.java
+++ app/src/main/java/lt/pusnis/multibarcodereader/viewmodels/MainViewModel.java
@@ -1,33 +1,33 @@
 package lt.pusnis.multibarcodereader.viewmodels;
 
 import android.util.Log;
-
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
+import java.util.List;
 import lt.pusnis.multibarcodereader.common.Constants;
 import lt.pusnis.multibarcodereader.common.network.MbrDataService;
 import lt.pusnis.multibarcodereader.common.network.MbrServiceClient;
-import lt.pusnis.multibarcodereader.common.network.RemoteRepository;
+import lt.pusnis.multibarcodereader.model.MbrFormats;
 import lt.pusnis.multibarcodereader.response.FormatsResponse;
-import lt.pusnis.multibarcodereader.response.TypesResponse;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainViewModel extends ViewModel {
 
-    MbrDataService service = MbrServiceClient.getUserInstance().create(MbrDataService.class);
-    //RemoteRepository remoteRepository = new RemoteRepository();
+    private MutableLiveData<List<MbrFormats>> formats;
+
 
     public void fetchAllFormats(){
-//        remoteRepository.getAllFormats();
         Log.i(Constants.LOG_TAG,"Formatų duomenys.");
+        MbrDataService service = MbrServiceClient.getUserInstance().create(MbrDataService.class);
 
         Call<FormatsResponse> call = service.getFormatList();
         Callback<FormatsResponse> callback = new Callback<FormatsResponse>() {
             @Override
             public void onResponse(Call<FormatsResponse> call, Response<FormatsResponse> response) {
                 Log.i(Constants.LOG_TAG, "" + response.body());
+                formats.postValue(response.body().getFormatData());
             }
 
             @Override
